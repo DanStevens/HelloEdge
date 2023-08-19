6b6421dd8cc33c15811004ac9d8a51fdca3ccc92
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2023-08-19 22:37:42 +0100

    Configured the project to enable Edge Single Plus mode for the app, including setting AndroidManifest.xml accordingly, and creating `EdgeWidgetProvider` class stub and `cocktail-provide` config file.

---
+++ app/src/main/java/com/example/helloedge/EdgeWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.example.helloedge;
+
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import com.samsung.android.sdk.look.cocktailbar.SlookCocktailManager;
+import com.samsung.android.sdk.look.cocktailbar.SlookCocktailProvider;
+
+public class EdgeWidgetProvider extends SlookCocktailProvider {
+}
